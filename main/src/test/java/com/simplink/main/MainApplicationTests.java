d9294767c811c3a33567c1c031dab060a3098003
Sevinc <sevincquliyeva000@gmail.com>
2026-05-12 18:11:19 +0400

    project created

---
+++ main/src/test/java/com/simplink/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplink.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
