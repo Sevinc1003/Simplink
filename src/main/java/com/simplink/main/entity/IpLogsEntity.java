90d2f4c22b7d2f8336d3b1f61c0dacbc5afec556
Sevinc <sevincquliyeva000@gmail.com>
2026-05-13 22:46:07 +0400

    entity and repo for IpLogs are created

---
+++ src/main/java/com/simplink/main/entity/IpLogsEntity.java
@@ -0,0 +1,55 @@
+package com.simplink.main.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ip_logs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IpLogsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "url_id", nullable = false)
+    private Url url;
+
+    @Column(nullable = false, length = 45)
+    private String ipAddress;
+
+    @Column(length = 500)
+    private String userAgent;
+
+    @Column(length = 2048)
+    private String referrer;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public IpLogsEntity(Url url, String ipAddress, String userAgent, String referrer) {
+        this.url = url;
+        this.ipAddress = ipAddress;
+        this.userAgent = userAgent;
+        this.referrer = referrer;
+    }
+
+}
